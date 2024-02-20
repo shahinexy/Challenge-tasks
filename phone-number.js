10ee018d03d55f08f076d1e6634e2139263941ce
shahinexy <shahinexy@gmail.com>
2024-02-20 14:11:28 +0600

    more validation added

--- phone-number.js
+++ phone-number.js
@@ -2,7 +2,8 @@
 
 // Users iccha moto jekono kichu dite pare, se +880 diye start korte pare, se country code na diyeo start korte pare 018.. 017 diye. se 11 digit er kom ba beshi dite pare. so all things should be taken into consideration.
 
-const number = +8801609729909;
+const number = +8801309729909;
+// console.log(number);
 function validNumber(number){
     if(!typeof number == 'number'){
         return 'Pleace Enter A Valid Number'
@@ -11,19 +12,32 @@ function validNumber(number){
     const numberStr = number.toString()
     const bangladeshCode = numberStr.slice(0,2)
     const onlyNumber = numberStr.slice(3);
-    if(numberStr.length === 13){
-        if(numberStr.startsWith('880')){
+
+    if(numberStr.length === 13 || numberStr.length == 10){
+        if(numberStr.length === 13){
+            if(numberStr.startsWith('880')){
+                let result = 'Invalid Operaotr'
+                for(let operatorNumber of allOperator){
+                    if(onlyNumber.startsWith(operatorNumber)){
+                        result = 'It\'s A Valid Bnagladeshi Number';
+                    }
+                }
+                return result;
+            }
+            else{
+                return 'Invalid Country Code'
+            }
+        }
+        else{
             let result = 'Invalid Operaotr'
             for(let operatorNumber of allOperator){
-                if(onlyNumber.startsWith(operatorNumber)){
+                if(numberStr.startsWith(operatorNumber)){
                     result = 'It\'s A Valid Bnagladeshi Number';
                 }
             }
             return result;
         }
-        else{
-            return 'Invalid Country Code'
-        }
+
     }
     else{
         return 'Invalid Number'
